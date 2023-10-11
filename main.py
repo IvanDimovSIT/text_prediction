d9e84487b6b2895662255319643aa52c097feec7
Ivan Dimov <100278096+IvanDimovSIT@users.noreply.github.com>
2023-10-11 16:25:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import random
 
+TRAINING_DATA_FILEPATH = "text.txt"
+OUTPUT_SIZE = 100
+
+
+def is_end_sentence(word) -> bool:
+    return word == "." or word == "?" or word == "!"
+
+
+def is_punctuation(word) -> bool:
+    return is_end_sentence(word) or word == ","
+
 
 class TokenRepository:
     def __init__(self) -> None:
-        self.id_counter = 0
+        self.id_counter: int = 0
         self.token_map = {}  # word to id map
 
     def register(self, word: str) -> None:
@@ -13,23 +24,24 @@ class TokenRepository:
         self.token_map.update({word: self.id_counter})
         self.id_counter += 1
 
-    def get_word(self, id: int) -> str:
+    def get_word(self, word_id: int) -> str:
         for key, val in self.token_map.items():
-            if val == id:
+            if val == word_id:
                 return key
 
-        return None
+        raise Exception("Id not found")
 
     def get_id(self, word: str) -> int:
         return self.token_map[word]
 
 
-class Tokenizer:
+class Scanner:
     def __init__(self) -> None:
         pass
 
-    def tokenize(self, repository: TokenRepository, text: str) -> list:  # list of ids of tokens
-        words = text.lower().replace("\n", " ").replace(",", " , ").replace(".", " . ").replace("?", " ? ").replace("-", " - ").replace("  ", " ").split(" ")
+    def scan(self, repository: TokenRepository, text: str) -> list:  # list of ids of tokens
+        words = text.lower().replace("\n", " ").replace(",", " , ").replace(".", " . ").replace("?", " ? ").\
+            replace("-", " - ").replace("  ", " ").split(" ")
         tokens = []
         for i in words:
             repository.register(i)
@@ -38,11 +50,11 @@ class Tokenizer:
         return tokens
 
 
-# to be used in a dict
+# all tokens seen before the current one
 class SingleTokenStatistics:
     def __init__(self) -> None:
         self.predecessor_tokens = {}  # dictionary of previous tokens and their counts
-        self.predecessor_count = 0  # total count of prev tokens
+        self.predecessor_count: int = 0  # total count of prev tokens
 
     def add(self, token: int) -> None:
         self.predecessor_count += 1
@@ -50,7 +62,7 @@ class SingleTokenStatistics:
             self.predecessor_tokens.update({token: 0})
         self.predecessor_tokens[token] += 1
 
-    def get_weight(self, predecessor_token: int) -> float:  # returns likelyhood
+    def get_weight(self, predecessor_token: int) -> float:  # returns likelihood
         if predecessor_token not in self.predecessor_tokens:
             return 0.0
 
@@ -63,8 +75,8 @@ class Predictor:
         self.token_stats = {}  # dict of token to SingleTokenStatistics
 
     def train(self, text: str) -> None:
-        tokenizer = Tokenizer()
-        tokens = tokenizer.tokenize(self.token_repository, text)
+        scanner = Scanner()
+        tokens = scanner.scan(self.token_repository, text)
 
         for i in range(0, len(tokens)):
             if i <= 0:
@@ -88,39 +100,45 @@ class Predictor:
 
         return rand_items[0]
 
-    def tokens_to_text(self, tokens: list) -> str:
+    def tokens_to_text(self, prompt: str, tokens: list) -> str:
         text = ""
         prev = ""
         for i in tokens:
             word = self.token_repository.get_word(i)
-            if prev == "." or prev == "?" or prev == "!":
+            if is_end_sentence(prev):
                 word = word.capitalize()
-            text += word
-            text += " "
-            
+            if prev == "":
+                prev = word
+                continue
             prev = word
 
-        return text
+            if not is_punctuation(word):
+                text += " "
+            text += word
+
+        return prompt + text
 
     def generate_text(self, text: str, length: int = 30) -> str:
-        tokens = Tokenizer().tokenize(self.token_repository, text)
+        tokens = Scanner().scan(self.token_repository, text)
         generated_tokens = []
-        generated_tokens.append(tokens[len(tokens)-1])
-        for i in range(length-1):
-            generated_tokens.append(self.get_next(generated_tokens[len(generated_tokens)-1]))
+        generated_tokens.append(tokens[len(tokens) - 1])
+        for i in range(length - 1):
+            generated_tokens.append(self.get_next(generated_tokens[len(generated_tokens) - 1]))
 
-        return self.tokens_to_text(generated_tokens)
+        return self.tokens_to_text(text, generated_tokens)
 
 
 def main():
-    with open("text.txt", "r", encoding='utf-8') as file:
+    with open(TRAINING_DATA_FILEPATH, "r", encoding="utf-8") as file:
         lines = file.readlines()
         lines = [line.strip() for line in lines]
 
     test_str = " ".join(lines)
     predictor = Predictor()
     predictor.train(test_str)
-    print(predictor.generate_text(input(">"), 100))
+    print(predictor.generate_text(input(">"), OUTPUT_SIZE))
+
 
 if __name__ == "__main__":
     main()
+
